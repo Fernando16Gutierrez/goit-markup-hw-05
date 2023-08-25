8ac3f0d1234520feae778db7a99e553896c78717
fernando <nathacanos62@gmail.com>
2023-08-24 23:03:09 -0500

    css-index

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,14 @@
-let cerrar = document.querySelectorAll(".data-modal-close")[0];
-let abrir = document.querySelectorAll(".data-modal-open")[0];
-let modal = document.querySelectorAll(".modal")[0];
-let dataM = document.querySelectorAll(".data-modal")[0];
-
-abrir.addEventListener("click", function(e){
-e.preventDefault();
-// @ts-ignore
-dataM.style.opacity = "1";
-// @ts-ignore
-dataM.style.visibility = "visible";
-modal.classList.toggle("modal-close");
-});
-
-cerrar.addEventListener("click", function(){
-    modal.classList.toggle("modal-close");
-
-    setTimeout(function(){
-    // @ts-ignore
-        dataM.style.opacity = "0";
-        // @ts-ignore
-        dataM.style.visibility("hidden");
-    }, 1000)
-    } )
-    
-
-    
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
